ae1f38f41f909c907f7f072800ec8b7c7f5a2278
github_awkwardlynumb <pearce.matthewa@gmail.com>
2020-10-06 07:21:48 -0700

    other pages added with header and footer, content to come

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,4 @@
-$(".banner").css({ height: $(window).height() + "px" });
+$(".banner").css({ height: $(window).height() - 100 + "px" });
 
 $(window).on("resize", function () {
   $(".banner").css({ height: $(window).height() + "px" });
