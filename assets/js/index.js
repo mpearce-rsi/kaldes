0e6eadd5f48bc5159242f6666b278732079e5c12
awkwardlynumb <pearce.matthewa@gmail.com>
2021-04-17 16:48:45 -0600

    YT complete on new Media page, staging for progress report

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,3 +4,7 @@ $(window).on("resize", function () {
   $(".banner").css({ height: $(window).height() + "px" });
 });
 
+const youTubeHeight = $("#youtubedesc").height()
+console.log(youTubeHeight)
+
+$("#ytFrame").css("height", youTubeHeight)
